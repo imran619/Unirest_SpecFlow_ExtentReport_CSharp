882ead6de5997b5cafbf26d041ea4aab07f8482a
imran619 <rifay.wizkid101@gmail.com>
2019-08-18 23:26:27 +0530

    REST API automation using Unirest with SpecFlow and Extent Reporting in CSharp.

---
+++ Unirest3/Unirest3/Utility/ReadExcel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unirest3.Utility;
+using System.IO;
+
+namespace Unirest3.Utility
+{
+    public class ReadExcel
+    {
+        public string filePathToExcel()
+        {
+            return @"E:\Imran\Selenium\CSProjects\UnirestProjects\Unirest3\Unirest3\Utility\data.xlsx";
+            //return Path.Combine(Environment.CurrentDirectory, @"Utility\", "data.xlsx");
+        }
+    }
+}
